9d1f1ac2cf70794bce955c208d80ae9d9432d9da
temujin05 <78592189+temujin05@users.noreply.github.com>
2023-04-24 15:41:07 +0800

    Add files via upload

---
+++ C++ program that only outputs the even digits  from 5 numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a,b,c,d,e;
+	cout<<"Please, input 5 numbers: ";
+	cin>>a>>b>>c>>d>>e;
+	if(a%2==0) {
+		cout<<a;
+	} 
+	if(b%2==0) {
+		cout<<b;
+	} 
+	if(c%2==0) {
+		cout<<c;
+	} 
+	if(d%2==0) {
+		cout<<d;
+	} 
+	if(e%2==0) {
+		cout<<e;
+	}
+	return 0;
+}
